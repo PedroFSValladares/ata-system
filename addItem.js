f0ccd675f7462da7939b15ff6db71e0c65dd12ee
Rubens Júnior <rubens.jr107@gmail.com>
2022-05-08 15:55:28 -0300

    dinamismo dos multiplos botoes

--- addItem.js
+++ addItem.js
@@ -1,64 +1,72 @@
-var campos;
-var capturando = "";
+let botoes = $('.botao');
+let inputs = $('.campo');
+var capturado = "";
 
-$('.btn').click((e)=>{
+$(inputs[1]).keypress(function (e) { 
+  if (e.which == 13) {
+    doSomething(e, 1);
+  }
+});
+
+$.each(botoes, (key,value) =>{
+  $(value).click(function (e) { 
+    doSomething(e, key);
+  });
+});
+
+function doSomething(e, key){
+  /* Retira o padrão do botão */
   e.preventDefault();
-  let btnAtual = e.currentTarget;
-  
-  if(btnAtual == $('.btn')[0]){
-    campos = $(':input').slice(0,2);
+
+  const input = inputs[key];
+  const lista = $('.lista')[key];
+
+  /* Responsável por capturar o dado */
+  envioDeDados(input, lista);
+
+  const btnExclui = $('.btnExclui');
+
+  exclusaoItens(btnExclui);
+}
+
+function envioDeDados(input, lista) { 
+  capturado= input.value;
+
+  /* Verifica se dados podem ser adicionados */
+  if(!capturado){
+    $(input).addClass("is-invalid").focus();
   }else{
-    campos = $(':input').slice(3,5);
-  }
-
-  let campo = campos[0];
-  capturando = campo.value;
-
-  while(capturando === "" && !campo.className.endsWith('is-invalid')){
-    campo.className += " is-invalid";
-    campo .focus();
-  }
-
-  function addItemsToDiv(lista){
     // Cria a div com os itens como inputs
     var arrayItems = ['div'];
     arrayLenght = arrayItems.length;
 
     for(i = 0; i < arrayLenght; i++){
         $('<div class="input-group mb-2">'
-            +'<input type="text" class="form-control" value="'+capturando+'">'
+            +'<input type="text" class="form-control" value="'+capturado+'">'
             +'<button class="btn btn-outline-danger btnExclui" type="button">Excluir</button>'
         +'</div>'
         ).appendTo(lista);
     }
 
-    // Zera o campo e da o foco a ele
-    campo.value = "";
-    campo.focus();
+    input.value = "";
+    input.focus();
   }
+}
 
-  if(capturando && btnAtual == $('.btn')[0]){
-    addItemsToDiv($('.lista')[0]);
-  }else if(capturando && btnAtual == $('.btn')[1]){
-    addItemsToDiv($('.lista')[1]);
-  }
-
-
-  $(campo).keyup((e) => {
-    // Verifica se o alvo é o pauta e se ele possui a classe 'is-invalid'
-    if(e.target == campo && campo.className.endsWith('is-invalid')){
-      campo.classList.remove('is-invalid');
-  }
-  });
-
-  const botoes = $('.btnExclui');
-  
-  $.each(botoes, function (k,v) { 
+function exclusaoItens(btnExclui) {
+  $.each(btnExclui, function (k,v) { 
     $(v).click((e) =>{
       botaoAtual = e.currentTarget;
       divBotao = botaoAtual.parentNode;
       divBotao.remove();
     });
   });
-  
+}
+
+$.each(inputs, function (i, v) { 
+  $(v).keyup(function () { 
+    if($(v).hasClass("is-invalid")){
+      $(v).removeClass("is-invalid");
+    }
+  });
 });
