4b383374bbb87281e3cf2563806382c3e84c188e
Rubens Júnior <rubens.jr107@gmail.com>
2022-05-14 12:00:28 -0300

    updated final v1 project

--- index.php
+++ index.php
@@ -9,12 +9,10 @@
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
   <script src="https://kit.fontawesome.com/cf4fb9e680.js" crossorigin="anonymous"></script>
-  <script type="text/javascript" src="./jquery.js"></script>
+  <script type="text/javascript" src="./assets/js/jquery.js"></script>
 </head>
 
 <body>
-  <div id="resultado">
-  </div>
   <div class="container">
     <div class="row mt-5">
       <div class="col-lg-6 col-sm-12 mb-sm-3">
@@ -75,7 +73,7 @@
               </div>
               <div class="card-footer">
                 <div class="d-flex justify-content-end">
-                  <input class="btn btn-primary" id="visualizar" type="submit" value="Baixar" disabled>
+                  <input class="btn btn-primary" id="visualizar" type="submit" value="Continuar" disabled>
                 </div>
               </div>
             </form>
@@ -86,8 +84,8 @@
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  <script src="addItem.js"></script>
-  <script src="controleForm.js"></script>
+  <script src="./assets/js/addItem.js"></script>
+  <script src="./assets/js/controleForm.js"></script>
 </body>
 
 </html>
--- viewAta.php
+++ viewAta.php
@@ -7,26 +7,21 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Visualização do Documento</title>
 </head>
-<link rel="stylesheet" href="./view.css">
+<link rel="stylesheet" href="./assets/css/view.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-<script type="text/javascript" src="./jquery.js"></script>
+<script type="text/javascript" src="./assets/js/jquery.js"></script>
 
 <body>
-  <div id="resultado">
-  </div>
-  <p id="teste">
-  <p></p>
-  </p>
   <div class="container">
     <div class="row mt-5">
       <div class="col-lg-6 col-sm-12 mb-sm-3">
         <div class="conteudo">
-          <img src="./header.PNG" alt="Header" width="100%">
+          <img src="./assets/images/header.PNG" alt="Header" width="100%">
           <div class="meio">
             <div class="container aqui">
             </div>
           </div>
-          <img src="./footer.PNG" alt="Footer" width="100%">
+          <img src="./assets/images/footer.PNG" alt="Footer" width="100%">
         </div>
       </div>
       <div class="col-lg-6 col-sm-12 my-3 h-auto text-center">
@@ -35,7 +30,7 @@
             <a class="nav-link" href="#">Salvar</a>
           </li>
           <li class="nav-item">
-            <form action="./teste.php" method="post">
+            <form action="./document.php" method="post">
               <input type="submit" class="nav-link active" value="Baixar">
             </form>
           </li>
@@ -44,7 +39,7 @@
     </div>
   </div>
 
-  <script type="text/javascript" src="./getItemStorage.js"></script>
+  <script type="text/javascript" src="./assets/js/getItemStorage.js"></script>
 </body>
 
 </html>
